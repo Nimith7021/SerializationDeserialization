0e809e5e60dec3d5d9bb2a9efbcd406be545eb3d
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-23 22:01:47 +0530

    initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListOfObjectSerializeDeserialize.Models
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Id = {Id}\n" +
+                $"Name = {Name}\n" +
+                $"Age = {Age}\n";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Configuration;
+using System.Text.Json;
+using ListOfObjectSerializeDeserialize.Models;
+
+namespace ListOfObjectSerializeDeserialize
+{
+    internal class Program
+    {
+        static string path = ConfigurationManager.AppSettings["filePath"]!.ToString();
+        static List<Person> persons = new List<Person>()
+            {
+            new Person(1, "Ashish", 18),
+            new Person(2, "Ajay", 19),
+            new Person(3, "Binod", 20),
+            new Person(4, "Vijay", 25),
+            new Person(5, "Surya", 21)
+            };
+
+        static void Main(string[] args)
+        {
+            //Write();
+            if (File.Exists(path))
+            {
+                Read();
+            }
+            else
+            {
+                Console.WriteLine("Creating a Json File");
+                Write();
+            }
+            
+        }
+
+        static void Write()
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (Person person in persons)
+                {
+
+                    sw.WriteLine(JsonSerializer.Serialize(person));
+                }
+            }
+
+        }
+
+        static void Read()
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    Person person = JsonSerializer.Deserialize<Person>(sr.ReadLine());
+                    Console.WriteLine(person);
+                }
+            }
+        }
+    }
+}
